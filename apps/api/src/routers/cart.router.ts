938c733160460ccc95f8286300ae14bf199f0e2d
zawkey2412 <24.zawkey@gmail.com>
2024-10-05 10:16:58 +0700

    build: fix cart

--- apps/api/src/routers/cart.router.ts
+++ apps/api/src/routers/cart.router.ts
@@ -3,22 +3,39 @@ import { CartController } from "@/controllers/cart.controller";
 import { AuthMiddleware } from "@/middleware/auth.middleware";
 
 class CartRouter {
-    public router: Router;
-    private cartController: CartController;
-    private authMiddleware: AuthMiddleware;
+  public router: Router;
+  private cartController: CartController;
+  private authMiddleware: AuthMiddleware;
 
-    constructor() {
-        this.router = Router();
-        this.cartController = new CartController();
-        this.authMiddleware = new AuthMiddleware();
-        this.initializeRoutes();
-    }
+  constructor() {
+    this.router = Router();
+    this.cartController = new CartController();
+    this.authMiddleware = new AuthMiddleware();
+    this.initializeRoutes();
+  }
 
-    private initializeRoutes() {
-        this.router.post('/add', this.authMiddleware.verifyToken, this.cartController.addToCart);
-        this.router.patch('/update', this.authMiddleware.verifyToken, this.cartController.updateCart);
-        this.router.delete('/remove', this.authMiddleware.verifyToken, this.cartController.removeFromCart);
-    }
+  private initializeRoutes() {
+    this.router.post(
+      "/add",
+      this.authMiddleware.verifyToken,
+      this.cartController.addToCart
+    );
+    this.router.patch(
+      "/update",
+      this.authMiddleware.verifyToken,
+      this.cartController.updateCart
+    );
+    this.router.delete(
+      "/remove",
+      this.authMiddleware.verifyToken,
+      this.cartController.removeFromCart
+    );
+    this.router.get(
+      "/count/:user_id",
+      this.authMiddleware.verifyToken,
+      this.cartController.getCartCount
+    );
+  }
 }
 
 export default new CartRouter().router;
